56bf3c8589cda1e9391b93f85a3d1779f2e1e0cc
Steven Lee <stevelee83@gmail.com>
2023-04-11 16:30:46 -0700

    add html code and search button functionality

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 console.log("Let's get this party started!");
+
+// https://api.giphy.com/v1/gifs/search?api_key=nIfOgRsfVgS8TJOBkvYB2r8L51kOLWBC
+// &q=&limit=1&offset=0&rating=pg&lang=en
+
+const searchGifs = $('#search-button');
+const searchTerm = $('#search-term').val();
+const gifBox = $('#gifs');
+const deleteGifs = $('#delete-button');
+
+const returnGif = async function() {
+  let gif = await axios.get("https://api.giphy.com/v1/gifs/search", {params: {
+    api_key: nIfOgRsfVgS8TJOBkvYB2r8L51kOLWBC,
+    q: searchTerm,
+    limit: 1,
+    offset: 0,
+    rating: pg,
+    lang: en
+  }});
+  gifBox.html(gif);
+}
+
+searchGifs.on("click", returnGif);
+
