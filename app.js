3ae207fdb3975895d3a78905dc863390e10de02c
Crynowecer0 <crynowecer11@gmail.com>
2023-04-12 11:59:21 -0400

    progress on persistance - gif objects in storage

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 "use strict";
 
-console.log("Let's get this party started!");
+console.log(localStorage)
+
+renderStoredGifs()
 
 /** global constants */
 //API would normally never be pushed to github
@@ -17,35 +19,65 @@ const $deleteGifs = $("#delete-button");
 $searchGifs.on("click", handleClick);
 $deleteGifs.on("click", deleteAllGifs);
 
-//getGif function -> gets the gif
-async function getGif(){
+/** makes API call and returns a single gif object */
+async function getGif() {
   let searchTerm = $searchTerm.val();
   let gif = await axios.get(`${BASE_URL}`, {
     params: {
       api_key: `${API_KEY}`,
       tag: `${searchTerm}`,
-      rating: 'g'
+      rating: "pg",
     },
   });
 
-  return gif
+  return gif;
 }
 
-//addGifToDom -> builds the gif node and adds it to the dom
-function addGifToDom(gif) {
-  const newGifSource = gif.data.data.images.original.url;
-  const $newGifElement = $("<img>", { src: newGifSource });
+/**builds a gif element and appends that element to the dom */
+function addGifToDom(gifObject) {
+  storeGif(gifObject);
+  const newGifImage = gifObject.data.data.images.original.url;
+  const newGifId = gifObject.data.data.id;
+
+  const $newGifElement = $("<img>", { src: newGifImage });
+  $newGifElement.addClass("gif");
+  $newGifElement.attr("id", `${newGifId}`);
 
   $gifBox.append($newGifElement);
+
   $searchTerm.val("");
 }
 
-//deleteGifAllGifs -> clears the gifs
+/**deletes all of the gifs currently displayed */
 function deleteAllGifs() {
   $gifBox.empty();
 }
 
-async function handleClick() {
-  const gif = await getGif()
-  addGifToDom(gif)
+/**stores the entire gifObject returned from getGif in local storage
+ * uses ID from API as key
+ */
+function storeGif(gifObject) {
+  const gifId = gifObject.data.data.id;
+  localStorage.setItem(`${gifId}`, JSON.stringify(gifObject));
 }
+
+/**renders all gifs stored in local storage on load, allowing data to persist */
+function renderStoredGifs() {
+  for (let item of localStorage) {
+    console.log('item=', item)
+  }
+}
+
+/**controller function that calls getGif and adds returned gif to the dom */
+async function handleClick() {
+  const gif = await getGif();
+  addGifToDom(gif);
+}
+
+//add local storage for persistence. Focus on one task at once
+//for local storage
+//need to add it on creation and clear local storage on delete all
+
+//allow deletion of individual gifs
+//allow voting on gifs -> tied to local storage?
+//style the page
